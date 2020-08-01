32ef6b530272abb8853d37a0c644796fcbd10980
surajsnanavare <surajsnanavare@gmail.com>
2020-08-01 14:21:01 +0530

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+Why Whatsapp yourself
+1. Make note of things e.g passwords, links, messages, ohotos,etc
+2. Check your profile from others point of view 
+3. List and check your groups
---
+++ assets/img/informme.co.in/jsm/coming-soon.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "mp4/bg.mp4",
+    poster: "img/bg-mobile-fallback.png"
+  }, {
+    posterType: 'png'
+  });
+
+})(jQuery); // End of use strict
